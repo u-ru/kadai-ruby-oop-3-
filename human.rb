26757b5e65eca7dbd6cad2e0ce28fe69b647cf92
yuuta.ootuka <yuuta.19950912@gmail.com>
2021-10-14 12:28:50 +0000

    kadai_oop3

---
+++ human.rb
@@ -0,0 +1,15 @@
+require "./animal"
+require "./thinkable"
+
+class Human < Animal
+  attr_accessor :hobby
+  
+  def initialize(name, age, hobby)
+    self.name = name
+    self.age = age
+    self.hobby = hobby
+  end
+  include Thinkable
+end
+  
+
